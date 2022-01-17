474e36bfbba721193de38fc0a283c31623d735a4
unknown <lasbreyp11@gmai.com>
2022-01-17 13:05:49 +0100

    commit

---
+++ animal.java
@@ -0,0 +1,27 @@
+class Animal{
+
+    static string name;
+
+    public static void main(string){}args{
+        setName("lasbrey paul");
+        string text = withdraw(260000);
+        system.out.println(text);
+    }
+
+    static void setName(string -name){
+        name = name;
+    }
+
+    string getName(){
+        return name;
+    }
+
+    static string withdraw(int amont){
+        int balance =250000;
+        if(amount>balance){
+            return"insuficient balance";
+        }else{int currentBalance =balance-amount;
+        return "your current balance is"+currentBalance;}
+        
+    }
+}
---
+++ vehicle.java
@@ -0,0 +1,32 @@
+pulic class Vehicle{
+
+    string name;
+    string manufacturer;
+    string model;
+    string dateofmanufacturing;
+    string enginenumber;
+
+
+    public void setName(string name){
+        this.name=name;
+    }
+
+    public void setManufacturer(string manufacturer){
+        this.manufacture=manufacturer;
+    }
+
+    public viod setModel(string model){
+        this.model=model;
+    }
+
+    public void setDateofmanufacturing(string dateofmanufacturing){
+        this.dateofmanufacturing=dateofmanufacturinenginenumber;
+    }
+
+    public void setEnginenumber(int enginenumber){
+        this.enginenumber=enginenumber;
+    }
+
+
+ 
+}
