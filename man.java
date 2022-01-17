e675ceff682c4db3d6c82d506d6117188d4be0e9
lasbreypaul <lasbreyp11@gmail.com>
2022-01-17 13:30:48 +0100

    commit

---
+++ man.java
@@ -0,0 +1,36 @@
+public class Man{
+
+    string name;
+    string address;
+    string nationality;
+    string dateofbirth;
+    string placeofbirth;
+    string stateoforigin;
+
+
+    public void setName(string name){
+        this.name =name;
+    }
+
+
+    public void setAddress(string address){
+        this.address = address;
+    }
+
+
+    public void setNationality(string nationality){
+        this.natioinality = nationality;
+    }
+
+
+
+    public void setDateofbirth(string dateofbirth){
+        this.dateofbirth = dateofbirth;
+    }
+
+
+
+    public void setStateoforigin(string stateoforigin){
+        this.stateoforigin;
+    }
+}
---
+++ schools.java
@@ -0,0 +1,11 @@
+pulic class Sch00l{
+
+    string name;
+    string address;
+    string type;
+    string dateofestablishment
+    
+
+
+
+}
